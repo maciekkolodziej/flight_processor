7351ac2f6c812d5dea69213cf415a88a27d659a4
Maciej Kołodziej <mkolodziej@grupainteger.pl>
2015-10-31 15:22:31 +0100

    Adds carrier code validations and identification.

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,19 +3,20 @@ class Flight
   include ActiveModel::Validations
 
   attribute :id, String
+  attribute :carrier_code_type, String
   attribute :carrier_code, String
   attribute :flight_number, String
   attribute :flight_date, Date
 
   validates :id, :carrier_code, :flight_number, :flight_date, presence: true
   validate :validate_date
+  validate :validate_carrier_code
 
-  def carrier_code_type
-    'TYPE'
-  end
+  ICAO_REGEXP = /\A[a-zA-Z]{3}\z/.freeze
+  IATA_REGEXP = /\A[a-zA-Z\d]{2}\*?\z/.freeze
 
   def to_a
-    [id, carrier_code, flight_number, flight_date]
+    [id, carrier_code_type, carrier_code, flight_number, flight_date]
   end
 
   def to_invalid_a
@@ -26,5 +27,15 @@ class Flight
 
   def validate_date
     errors.add(:flight_date, 'is not a valid date') unless flight_date.is_a? Date
+  end
+
+  def validate_carrier_code
+    if carrier_code =~ ICAO_REGEXP
+      self.carrier_code_type = 'ICAO'
+    elsif carrier_code =~ IATA_REGEXP
+      self.carrier_code_type = 'IATA'
+    else
+      errors.add(:carrier_code, :invalid)
+    end
   end
 end
